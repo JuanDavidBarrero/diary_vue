0c0720b61b138202b9f639993b2573724d4fccbd
Juan David Barrero <JuanDBarrero@hotmail.com>
2021-11-03 16:52:37 -0500

    Sidebar added

--- src/modules/daybook/components/Sidebar.vue
+++ src/modules/daybook/components/Sidebar.vue
@@ -1,12 +1,26 @@
 <template>
   <aside class="daybook__sidebar">
-      sidebar
+    <input type="text" placeholder="Buscar entry" />
+
+    <div class="daybook__entry" v-for="item in 10" :key="item">
+      <div class="entry">
+          <div class="daybook__entrytile">
+            <span>15</span>
+            <span>Julio</span>
+            <p>2021,jueves</p>
+          </div>
+          <div class="daybook__entry__body">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
+            provident ullam voluptas accusantium aspernatur beatae, ab quas itaque
+            tenetur corporis, repudiandae id sint culpa quae aliquam ipsam
+            exercitationem, fugiat illo.
+          </div>
+      </div>
+    </div>
   </aside>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
