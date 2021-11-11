eb91f5782dab19f49f63730684c815d3c978d5b4
Juan David Barrero <JuanDBarrero@hotmail.com>
2021-11-11 13:06:40 -0500

    start working with vuex

---
+++ src/modules/daybook/store/journal/getters.js
@@ -0,0 +1,13 @@
+
+// export const myGetter = (/* state */) => {
+//  return /* state */;
+// }
+
+
+
+export const getEnryByTerm = (/* state */) => {
+    return /* state */;
+}
+export const getEntryById = (/* state */) => {
+    return /* state */;
+}
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,24 @@
+
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus iaculis urna id volutpat. Et pharetra pharetra massa massa ultricies mi quis. Dictum fusce ut placerat orci. Ligula ullamcorper malesuada proin libero nunc consequat. Convallis aenean et tortor at risus viverra adipiscing at in. Odio morbi quis commodo odio aenean sed adipiscing. Maecenas ultricies mi eget mauris pharetra et. Ante metus dictum at tempor commodo ullamcorper a lacus. Neque laoreet suspendisse interdum consectetur. In mollis nunc sed id semper risus in hendrerit gravida. Risus in hendrerit gravida rutrum quisque. Mauris cursus mattis molestie a iaculis. Vitae elementum curabitur vitae nunc sed velit. Ultricies integer quis auctor elit. Tempus quam pellentesque nec nam aliquam sem et tortor. A iaculis at erat pellentesque.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus iaculis urna id volutpat. Et pharetra pharetra massa massa ultricies mlesuada proin libero nunc consequat. Convallis aenean et tortor at risus viverra adipiscing at in. Odio morbi aretra et. Ante metus dictum at tempor commodo ullamcorper a lacus. Neque laoreet suspendisse interdum consectetur. In mollis nunc sed id semper risus in hendrerit gravida. Risus in hendrerit gravida rutrum quisque. Mauris cursus mattis molesti. Tempus quam pellentesque nec nam aliquam sem et tortor. A iaculis at erat pellentesque.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus iaculis urna id volutpat. Et pharetra pharetra massa massa ultricies mi quis. Dictum fusce ut placerat orci. Ligula ullamcorper malesuada proin libero nunc consequat. Convallis aenean et tortor at risus viverra adipiscing at in. Odio morbi quis commodo tus dictum at tempor commodo ullamcorper a lacus. Neque laoreet suspendisse interdum consectetur. In mollis nunc sed id semper risus in hendrerit gravida. Risus in s cursus mattis molestie a iaculis. Vitae elementum curabitur vitae nunc sed velit. Ultricies integer quis auctor elit. Tempus quam pellentesque nec nam aliquam sem et tortor. A iaculis at erat pellentesque.',
+            picture: null,
+        },
+    ]
+})
